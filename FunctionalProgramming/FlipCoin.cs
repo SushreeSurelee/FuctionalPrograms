31ebbc27fc5f7297ee8bb6be0919ad685861eef2
“Sushree” <sushreesureleesahu98@gmail.com>
2022-08-03 21:42:55 +0530

    [Sushree | Added fuctionality to calculate Percentage of head and tail in coin flips]

---
+++ FunctionalProgramming/FlipCoin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    class FlipCoin
+    {
+        public static void FlipACoin(int turn)
+        {
+            Random random = new Random();
+            if (turn > 0)
+            {
+                int head = 0;
+                int tail = 0;
+                for (int i = 1; i <= turn; i++)
+                {
+                    int flipCheck = random.Next(0, 2);
+                    if (flipCheck == 1)
+                    {
+                        head++;
+                    }
+                    else
+                    {
+                        tail++;
+                    }
+                }
+                double percentageOfHeads = (head * 100) / turn;
+                Console.WriteLine("Number of time head come after fliping the coin is : {0}\nThe percentage of Heads is : {1}%", head, percentageOfHeads);
+                double percentageOfTails = (tail * 100) / turn;
+                Console.WriteLine("Number of time tail come after fliping the coin is : {0}\nThe percentage of Tails is : {1}%", tail, percentageOfTails);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a positive value to flip the coin.");
+            }
+        }
+    }
+}
+
+
--- FunctionalProgramming/Program.cs
+++ FunctionalProgramming/Program.cs
@@ -10,7 +10,21 @@ namespace FunctionalProgramming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Fuctional Programming application.");
+            Console.WriteLine("Welcome to Fuctional Programming application.\n");
+            Console.WriteLine("Please choose number between 1 to 10 as shown below\n");
+            Console.WriteLine("1:Flip a coin");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the Number of time you would like to flip the coin");
+                    int numOfTurns = Convert.ToInt32(Console.ReadLine());
+                    FlipCoin.FlipACoin(numOfTurns);
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid number. Terminating application");
+                    break;
+            }
             Console.ReadLine();
         }
     }
