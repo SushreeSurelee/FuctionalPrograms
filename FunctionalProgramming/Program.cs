b5c162b4ed143aa1f7749b1d9a9c6bac514b7027
“Sushree” <sushreesureleesahu98@gmail.com>
2022-08-03 21:17:11 +0530

    [Sushree | Initial Commit]

---
+++ FunctionalProgramming/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Fuctional Programming application.");
+            Console.ReadLine();
+        }
+    }
+}
+
